800d2c8f81d3611afc5ec90ce5727412bf6358bb
chromodyne <dsinitiere@outlook.com>
2022-06-29 19:12:32 -0700

    Implemented basic time/date retrieval using momentjs API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 //Used for time with MomentJS API.
 let m = moment();
+
+//Gets the current time from the momentJS api.
+let currentTime = m.format("LT");
+
+//Changes header text to show current weekday, month, and day using moment.js
+document.getElementById("currentDay").textContent = m.format("dddd, MMMM Do");
+
+console.log(currentTime);
