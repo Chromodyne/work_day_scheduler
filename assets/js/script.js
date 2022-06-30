cd14b5e471034572a5a51e5c875e40b1ed58281b
chromodyne <dsinitiere@outlook.com>
2022-06-29 21:11:06 -0700

    Began implementing coloring logic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 //Used for time with MomentJS API.
 let m = moment();
 
-//Gets the current time from the momentJS api.
-let currentTime = m.format("LT");
+//Gets the current hour from the momentJS api to determine description colors.
+let currentHour = m.hour();
 
 //Changes header text to show current weekday, month, and day using moment.js
 document.getElementById("currentDay").textContent = m.format("dddd, MMMM Do");
 
-console.log(currentTime);
+// setAllColors();
+
+//This function changes the colors of entry based on current time.
+//This uses JQuery, trying to avoid for performance.
+// $(function() {
+
+//     $(this).find(".description").addClass("present");
+
+// });
+
+const des = document.querySelectorAll(".description");
+
+for (const desc of des) {
+    desc.classList.add("past");
+}
